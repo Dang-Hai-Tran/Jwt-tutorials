0fbd78f2a49752b204017670443320f024ea43c6
harry <danghai.ema@gmail.com>
2022-05-02 01:25:38 +0200

    1st commit: Create frontend and backend env

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("CONNECTED TO MONGODB SUCCESSFULLY.")
+})
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+const port = 8000;
+
+// Routes
+app.use("/v1/auth", authRoute)
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
